ae45c8793df4c9c39597800caaeaf1b988664d82
Hares <alharsb15@gmail.com>
2025-05-28 14:04:52 +0300

    fixes

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -12,8 +12,8 @@ function Page() {
     };
 
     return (
-        <div className="  h-full  min-h-[100dvh] w-full grid items-center mx-auto  justify-center relative">
-            <div className="relative z-20 w-full md:min-w-[500px]  animate-fadeIn container">
+        <div className="  h-full  min-h-[100dvh] w-full flex items-center mx-auto  justify-center relative">
+            <div className="relative z-20 w-full md:min-w-[500px]  animate-fadeIn container max-md:px-4">
                 <LoginForm onSubmit={handleLogin} />
             </div>
         </div>
